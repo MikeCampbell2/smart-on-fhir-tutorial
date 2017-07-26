b66d6cfb1b5079aca985abaee395b9442b322c2f
MikeCampbell2 <mike.campbell2@dxc.com>
2017-07-26 15:46:19 -0400

    a

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -4,7 +4,7 @@
 
     function onError() {
       console.log('Loading error', arguments);
-      //mec...hmmm...alert('Loading Error: ' + arguments);
+      alert('Loading Error: ' + arguments);
       ret.reject();
     }
 
@@ -12,72 +12,72 @@
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
         var pt = patient.read();
-        //var obv = smart.patient.api.fetchAll({
-        //            type: 'Observation',
-        //            query: {
-        //              code: {
-        //                $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-        //                      'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-        //                      'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
-        //              }
-        //            }
-        //          });
-        //
-        //
-        //$.when(pt, obv).fail(onError);
-        //
-        //$.when(pt, obv).done(function(patient, obv) {
-        //  var byCodes = smart.byCodes(obv, 'code');
-        //  var gender = patient.gender;
-        //  var dob = new Date(patient.birthDate);
-        //  var day = dob.getDate() + 1; //mec... bug off by 1 day (counts from 0 ???)
-        //  var monthIndex = dob.getMonth() + 1;
-        //  var year = dob.getFullYear();
-        //
-        //  var dobStr = monthIndex + '/' + day + '/' + year;
-        //  var fname = '';
-        //  var lname = '';
-        //
-        //  if (typeof patient.name[0] !== 'undefined') {
-        //    fname = patient.name[0].given.join(' ');
-        //    lname = patient.name[0].family.join(' ');
-        //  }
-        //
-        //  var height = byCodes('8302-2');
-        //  var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
-        //  var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
-        //  var hdl = byCodes('2085-9');
-        //  var ldl = byCodes('2089-1');
-        //
-        //  var p = defaultPatient();
-        //  p.birthdate = dobStr;
-        //  p.gender = gender;
-        //  p.fname = fname;
-        //  p.lname = lname;
-        //  p.age = parseInt(calculateAge(dob));
-        //  p.height = getQuantityValueAndUnit(height[0]);
-        //
-        //  if (typeof systolicbp != 'undefined')  {
-        //    p.systolicbp = systolicbp;
-        //  }
-        //
-        //  if (typeof diastolicbp != 'undefined') {
-        //    p.diastolicbp = diastolicbp;
-        //  }
-        //
-        //  p.hdl = getQuantityValueAndUnit(hdl[0]);
-        //  p.ldl = getQuantityValueAndUnit(ldl[0]);
-        //  alert('mec...here...('+ patient.id +',' + patient.name[0].family + ',' + patient.birthDate + ',' + patient.name[0].use +',' + patient.name[0].period.start + ',' + patient.careProvider[0].display + ')');
-        //  //var byMec = smart.byCodes(obv, 'effectiveDateTime');
-        //  var mmm = height[0].effectiveDateTime;
-        //
-        //  alert('mec...here...mmm...('+ mmm + ')');
-        //  var mec = 'bogus...';
-        //  //var mec = obv.getAttribute('status'); //mec... smart.getDate(); //mec...whoa...
-        //  p.ldl = mec; //mec... diastolicbp; //mec... WHOA!!!
-        //
-        //  ret.resolve(p);
-        //});
+        var obv = smart.patient.api.fetchAll({
+                    type: 'Observation',
+                    query: {
+                      code: {
+                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
+                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
+                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                      }
+                    }
+                  });
+
+
+        $.when(pt, obv).fail(onError);
+
+        $.when(pt, obv).done(function(patient, obv) {
+          var byCodes = smart.byCodes(obv, 'code');
+          var gender = patient.gender;
+          var dob = new Date(patient.birthDate);
+          var day = dob.getDate() + 1; //mec... bug off by 1 day (counts from 0 ???)
+          var monthIndex = dob.getMonth() + 1;
+          var year = dob.getFullYear();
+
+          var dobStr = monthIndex + '/' + day + '/' + year;
+          var fname = '';
+          var lname = '';
+
+          if (typeof patient.name[0] !== 'undefined') {
+            fname = patient.name[0].given.join(' ');
+            lname = patient.name[0].family.join(' ');
+          }
+
+          var height = byCodes('8302-2');
+          var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
+          var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
+          var hdl = byCodes('2085-9');
+          var ldl = byCodes('2089-1');
+
+          var p = defaultPatient();
+          p.birthdate = dobStr;
+          p.gender = gender;
+          p.fname = fname;
+          p.lname = lname;
+          p.age = parseInt(calculateAge(dob));
+          p.height = getQuantityValueAndUnit(height[0]);
+
+          if (typeof systolicbp != 'undefined')  {
+            p.systolicbp = systolicbp;
+          }
+
+          if (typeof diastolicbp != 'undefined') {
+            p.diastolicbp = diastolicbp;
+          }
+
+          p.hdl = getQuantityValueAndUnit(hdl[0]);
+          p.ldl = getQuantityValueAndUnit(ldl[0]);
+          alert('mec...here...('+ patient.id +',' + patient.name[0].family + ',' + patient.birthDate + ',' + patient.name[0].use +',' + patient.name[0].period.start + ',' + patient.careProvider[0].display + ')');
+          //var byMec = smart.byCodes(obv, 'effectiveDateTime');
+          var mmm = height[0].effectiveDateTime;
+
+          alert('mec...here...mmm...('+ mmm + ')');
+          var mec = 'bogus...';
+          //var mec = obv.getAttribute('status'); //mec... smart.getDate(); //mec...whoa...
+          p.ldl = mec; //mec... diastolicbp; //mec... WHOA!!!
+
+          ret.resolve(p);
+        });
 
 
         //VVV mec...yoyo...
@@ -91,8 +91,8 @@
             code: {
               $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
                     'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                    'http://loinc.org|2089-1', 'http://loinc.org|55284-4',
-                //'2017-02-23', '2017-07-19',
+                    'http://loinc.org|2089-1', 'http://loinc.org|55284-4'
+                //'2017-02-23', '2017-07-19'
               ]
             }
 
