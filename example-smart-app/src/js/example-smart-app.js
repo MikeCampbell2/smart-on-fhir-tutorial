1a426441e634a6e4cff8f2da2d969b3e3672889e
MikeCampbell2 <mike.campbell2@dxc.com>
2017-08-02 08:13:53 -0400

    a

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -306,7 +306,8 @@
   // VVV ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Patient ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   function updatePatient(smart, pt) {
     alert('mec...TOP... in UPDATE patient (' + JSON.stringify(smart.patient) + ')');
-    alert('mec...222... in UPDATE patient (' + pt.resourceType + ',' + pt + ',' + JSON.stringify(pt) + ')');
+    alert('mec...333... in UPDATE SMART patient (' + smart.patient.resourceType + ',' + smart.patient + ',' + JSON.stringify(smart.patient) + ')');
+    alert('mec...xxx... in UPDATE patient (' + pt.resourceType + ',' + pt + ',' + JSON.stringify(pt) + ')');
     //pt.name[0].family[0] = "NewName";
     pt.birthDate = "1946-08-22"; //ORG
     pt.birthDate = "1946-08-24"; // NEW- DO NOT KEEP THIS, it is the WRONG birthdata
